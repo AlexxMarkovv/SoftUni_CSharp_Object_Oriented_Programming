dfff027bef937d0600655b5b0333e417a41239f6
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-12-21 11:53:03 +0200

    Create RaceMotorcycle.cs

---
+++ INHERITANCE LAB AND EXERCISE/04.NeedForSpeed/RaceMotorcycle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        private const double defaultFuelConsumption = 8;
+        public RaceMotorcycle(int horsePower, double fuel)
+           : base(horsePower, fuel)
+        {
+
+        }
+
+        public override void Drive(double kilometers)
+        {
+            double fuelSum = defaultFuelConsumption * kilometers;
+
+            Fuel -= fuelSum;
+            if (Fuel < 0)
+            {
+                Fuel = 0;
+            }
+        }
+    }
+}
