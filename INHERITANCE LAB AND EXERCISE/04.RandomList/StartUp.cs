9170098fb75d8311fbe31e046f21cd54c27f13f3
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-12-21 11:54:00 +0200

    Create StartUp.cs

---
+++ INHERITANCE LAB AND EXERCISE/04.RandomList/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace CustomRandomList;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        RandomList list = new RandomList()
+        {
+            { "One" },
+            { "Two" },
+            { "Three" },
+            { "Four" },
+        };
+
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine(list.RandomString());
+    }
+}
