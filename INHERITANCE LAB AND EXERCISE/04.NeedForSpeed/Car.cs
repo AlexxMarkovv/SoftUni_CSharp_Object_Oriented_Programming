06e1a650cd7a050cc3f96a5cf7f12e8957ee642d
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-12-21 11:52:12 +0200

    Create Car.cs

---
+++ INHERITANCE LAB AND EXERCISE/04.NeedForSpeed/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public class Car : Vehicle
+    {
+        private const double defaultFuelConsumption = 3;
+
+        public Car(int horsePower, double fuel)
+           : base(horsePower, fuel)
+        {
+
+        }
+
+        public override void Drive(double kilometers)
+        {
+            double fuelSum = defaultFuelConsumption * kilometers;
+
+            Fuel -= fuelSum;
+            if (Fuel < 0)
+            {
+                Fuel = 0;
+            }
+        }
+    }
+}
