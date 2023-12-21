219b108d0bbb736ee7969c8b6548eed0a6886ddc
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-12-21 11:53:22 +0200

    Create StartUp.cs

---
+++ INHERITANCE LAB AND EXERCISE/04.NeedForSpeed/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new(204, 55);
+
+            car.Drive(35);
+
+            Console.WriteLine(car.Fuel);
+        }
+    }
+}
