7ad2a2eb4df3fb3859ed0adc264a0f630121ba64
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-12-21 11:53:33 +0200

    Create Vehicle.cs

---
+++ INHERITANCE LAB AND EXERCISE/04.NeedForSpeed/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public abstract class Vehicle
+    {
+        private const double defaultFuelConsumption = 1.25;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+
+        public double DefaultFuelConsumption { get; set; }
+        public virtual double FuelConsumption { get; set; }
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+
+
+        public virtual void Drive(double kilometers)
+        {
+            double fuelSum = defaultFuelConsumption * kilometers;
+
+            Fuel -= fuelSum;
+
+            if (Fuel < 0)
+            {
+                Fuel = 0;
+            }
+        }
+    }
+}
