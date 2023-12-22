aaab5e272d2287f613df670f256264829c7306c1
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-12-22 11:48:37 +0200

    Create StartUp.cs

---
+++ INHERITANCE LAB AND EXERCISE/05.StackofStrings/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace CustomStack;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        StackOfStrings stack = new();
+
+        Console.WriteLine(stack.IsEmpty());
+
+        stack.AddRange(new List<string> { "Alex", "Gosho", "Marto" });
+
+        Console.WriteLine(string.Join(" ", stack));
+
+        Console.WriteLine(stack.Pop());
+    }
+}
