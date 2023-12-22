c5b920be9b31c35df9fe63502313000d8978691a
AlexxMarkovv <122829892+AlexxMarkovv@users.noreply.github.com>
2023-12-22 11:47:45 +0200

    Create StartUp.cs

---
+++ INHERITANCE LAB AND EXERCISE/05.Restaurant/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace Restaurant
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Starter startDish = new("Chips", 3.50m, 250);
+
+
+        }
+    }
+}
