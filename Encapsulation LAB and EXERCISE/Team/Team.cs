111d9979004ed4275d9f014ee8e56d221ed59f88
Alex Markov <langhorne05@gmail.com>
2023-12-28 09:01:48 +0200

    Create Team.cs

---
+++ Encapsulation LAB and EXERCISE/Team/Team.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            Name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public IReadOnlyList<Person> FirstTeam
+            => firstTeam.AsReadOnly();
+        
+        public IReadOnlyList<Person> ReserveTeam 
+            => reserveTeam.AsReadOnly();
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+    }
+}
