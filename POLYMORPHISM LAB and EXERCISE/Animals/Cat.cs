19cc983ac65135e9d7cde102474087840d495de5
Alex Markov <langhorne05@gmail.com>
2023-12-29 19:23:47 +0200

    Create Cat.cs

---
+++ POLYMORPHISM LAB and EXERCISE/Animals/Cat.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Text;
+
+namespace Animals
+{
+    public class Cat : Animal
+    {
+        public Cat(string name, string favouriteFood) 
+            : base(name, favouriteFood)
+        {
+        }
+
+        //I am Peter and my fovourite food is Whiskas
+        //MEEOW
+
+        public override string ExplainSelf()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"I am {Name} and my fovourite food is {FavoriteFood}");
+            sb.Append("MEEOW");
+
+            return sb.ToString();
+        }
+    }
+}
