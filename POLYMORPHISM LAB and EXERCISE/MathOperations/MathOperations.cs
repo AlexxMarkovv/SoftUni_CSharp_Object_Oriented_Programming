a752a39d6e7c07b137277e7d280e2c9bc281e259
Alex Markov <langhorne05@gmail.com>
2023-12-29 19:24:05 +0200

    Create MathOperations.cs

---
+++ POLYMORPHISM LAB and EXERCISE/MathOperations/MathOperations.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public double Add(double x, double y, double r)
+        {
+            return x + y + r;
+        }
+
+        public decimal Add(decimal x, decimal y, decimal r)
+        {
+            return x + y + r;
+        }
+    }
+}
