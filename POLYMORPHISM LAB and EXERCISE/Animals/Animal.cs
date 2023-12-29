db504788cefb8cf0e3a3e3cdf46ace866179c0c0
Alex Markov <langhorne05@gmail.com>
2023-12-29 19:23:39 +0200

    Create Animal.cs

---
+++ POLYMORPHISM LAB and EXERCISE/Animals/Animal.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+        private string favouriteFood;
+
+        public Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavoriteFood = favouriteFood;
+        }
+
+        protected string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        public string FavoriteFood
+        {
+            get { return favouriteFood; }
+            private set { favouriteFood = value;}
+        }
+
+        public abstract string ExplainSelf();
+    }
+}
