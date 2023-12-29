231b2a36523c69aa4812c6d11f26d832780f775a
Alex Markov <langhorne05@gmail.com>
2023-12-29 19:23:52 +0200

    Create Dog.cs

---
+++ POLYMORPHISM LAB and EXERCISE/Animals/Dog.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Text;
+
+namespace Animals
+{
+    public class Dog : Animal
+    {
+        public Dog(string name, string favouriteFood) 
+            : base(name, favouriteFood)
+        {
+        }
+
+        public override string ExplainSelf()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"I am {Name} and my fovourite food is {FavoriteFood}");
+            sb.Append("DJAAF");
+
+            return sb.ToString();
+        }
+    }
+}
