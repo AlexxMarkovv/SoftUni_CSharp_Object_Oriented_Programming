70991561c903298bb3d342f0a32a5f59f5c1891d
Alex Markov <langhorne05@gmail.com>
2023-12-30 08:28:05 +0200

    Create Druid.cs

---
+++ POLYMORPHISM LAB and EXERCISE/Raiding/Models/Druid.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Raiding.Models
+{
+    public class Druid : BaseHero
+    {
+        private int power = 80;
+        public Druid(string name, int power) : base(name, power)
+        {
+            Power = power;
+        }
+
+        public int Power
+        {
+            get => power;
+            set
+            {
+                power = value;
+            }
+        }
+    }
+}
