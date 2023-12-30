1061a2b92c9e0ae1fe54aebadbf057fb0a4bc737
Alex Markov <langhorne05@gmail.com>
2023-12-30 08:28:24 +0200

    Create Rectangle.cs

---
+++ POLYMORPHISM LAB and EXERCISE/Shapes/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public double Height
+        {
+            get { return height; }
+            private set
+            {
+                height = value;
+            }
+        }
+        public double Width
+        {
+            get { return width; }
+            private set
+            {
+                width = value;
+            }
+        }
+
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return (Height * 2) + (Width * 2);
+        }
+    }
+}
