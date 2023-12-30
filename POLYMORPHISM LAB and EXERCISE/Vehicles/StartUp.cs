59e38f37c4dcf2214c58f3537ea864642d88a228
Alex Markov <langhorne05@gmail.com>
2023-12-30 08:32:01 +0200

    Create StartUp.cs

---
+++ POLYMORPHISM LAB and EXERCISE/Vehicles/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using Vehicles.Core.Interfaces;
+using Vehicles.Core;
+using Vehicles.Factories.Interfaces;
+using Vehicles.Factories;
+using Vehicles.IO.Interfaces;
+using Vehicles.IO;
+
+IReader reader = new ConsoleReader();
+IWriter writer = new ConsoleWriter();
+IVehicleFactory vehicleFactory = new VehicleFactory();
+
+IEngine engine = new Engine(reader, writer, vehicleFactory);
+
+engine.Run();
