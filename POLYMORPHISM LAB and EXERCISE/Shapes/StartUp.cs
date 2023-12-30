0cfa8d0366edd19854fb5fa4dd98995de1ea7722
Alex Markov <langhorne05@gmail.com>
2023-12-30 08:28:43 +0200

    Create StartUp.cs

---
+++ POLYMORPHISM LAB and EXERCISE/Shapes/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(15);
+            Shape rectangle = new Rectangle(5, 10);
+
+            Console.WriteLine(circle.CalculatePerimeter());
+            Console.WriteLine(rectangle.CalculateArea());
+        }
+    }
+}
