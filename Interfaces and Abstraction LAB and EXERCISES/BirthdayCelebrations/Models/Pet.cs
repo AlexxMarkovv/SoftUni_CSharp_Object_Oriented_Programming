54ebac91d4916eece2f4a752fa459339f7c56234
Alex Markov <langhorne05@gmail.com>
2024-01-03 22:05:48 +0200

    Create Pet.cs

---
+++ Interfaces and Abstraction LAB and EXERCISES/BirthdayCelebrations/Models/Pet.cs
@@ -0,0 +1,17 @@
+﻿using BirthdayCelebrations.Models.Interfaces;
+
+namespace BirthdayCelebrations.Models
+{
+    public class Pet : ICelebratable
+    {
+        public Pet(string name, string birthday)
+        {
+            Name = name;
+            Birthdate = birthday;
+        }
+
+        public string Name { get; private set; }
+
+        public string Birthdate { get; private set; }
+    }
+}
