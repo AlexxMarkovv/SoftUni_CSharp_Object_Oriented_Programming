b20fdb579b9d23f25268140b1ae3111138dcbfc0
Alex Markov <langhorne05@gmail.com>
2024-01-04 10:46:42 +0200

    Create ICar.cs

---
+++ Interfaces and Abstraction LAB and EXERCISES/Cars/ICar.cs
@@ -0,0 +1,13 @@
+﻿namespace Cars
+{
+    public interface ICar
+    {
+        public string Model { get;}
+
+        public string Color { get; }
+
+        public void Start();
+
+        public void Stop();
+    }
+}
