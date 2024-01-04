4dbce30d620f9f3ff5b766ad63d36bfced0c7cf2
Alex Markov <langhorne05@gmail.com>
2024-01-04 10:47:06 +0200

    Create Tesla.cs

---
+++ Interfaces and Abstraction LAB and EXERCISES/Cars/Tesla.cs
@@ -0,0 +1,19 @@
+﻿namespace Cars
+{
+    public class Tesla : Car, IElectricCar
+    {
+        public Tesla(string model, string color, int battery) 
+            : base(model, color)
+        {
+            Battery = battery;
+        }
+
+        public int Battery { get; }
+
+        //Grey Seat Leon
+        public override string ToString()
+        {
+            return $"{Color} Tesla {Model} with {Battery} Batteries";
+        }
+    }
+}
