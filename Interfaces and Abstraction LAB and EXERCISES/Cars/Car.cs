0de861343af8dc31cf3115786a9c8846276061e4
Alex Markov <langhorne05@gmail.com>
2024-01-04 10:46:36 +0200

    Create Car.cs

---
+++ Interfaces and Abstraction LAB and EXERCISES/Cars/Car.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Cars
+{
+    public abstract class Car : ICar
+    {
+        public Car(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Model { get; }
+
+        public string Color { get; }
+
+        public void Start()
+        {
+            Console.WriteLine("Engine start");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Breaaak!");
+        }
+    }
+}
