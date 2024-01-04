856b88921b29e33f40fa9ecd520eafbd6feebaf7
Alex Markov <langhorne05@gmail.com>
2024-01-04 10:46:55 +0200

    Create Seat.cs

---
+++ Interfaces and Abstraction LAB and EXERCISES/Cars/Seat.cs
@@ -0,0 +1,17 @@
+﻿namespace Cars
+{
+    public class Seat : Car
+    {
+        public Seat(string model, string color) 
+            : base(model, color)
+        {
+
+        }
+
+        //Grey Seat Leon
+        public override string ToString()
+        {
+            return $"{Color} Seat {Model}";
+        }
+    }
+}
