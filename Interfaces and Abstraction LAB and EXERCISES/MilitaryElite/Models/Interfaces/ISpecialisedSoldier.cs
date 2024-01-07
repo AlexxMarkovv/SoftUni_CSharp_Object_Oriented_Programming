398030abe4784d4c9ff36921d32673c0f76a50ae
Alex Markov <langhorne05@gmail.com>
2024-01-07 09:23:24 +0200

    Create ISpecialisedSoldier.cs

---
+++ Interfaces and Abstraction LAB and EXERCISES/MilitaryElite/Models/Interfaces/ISpecialisedSoldier.cs
@@ -0,0 +1,10 @@
+﻿using MilitaryElite.Enums;
+
+
+namespace MilitaryElite.Models.Interfaces
+{
+    public interface ISpecialisedSoldier : IPrivate
+    {
+        public Corps Corps { get;}
+    }
+}
