7525ec59a4ff8772e319ece7cf9261dc9d0146cb
Alex Markov <langhorne05@gmail.com>
2024-01-08 20:09:22 +0200

    Create Smartphone.cs

---
+++ Interfaces and Abstraction LAB and EXERCISES/Telephony/Smartphone.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Telephony
+{
+    public class Smartphone : ICallable, IBrowsable
+    {
+        public string Call(string phoneNumber)
+        {
+            if (!ValidateNumber(phoneNumber))
+            {
+                throw new ArgumentException("Invalid number!");
+            }
+
+            return $"Calling... {phoneNumber}";
+        }
+
+        public string Browse(string website)
+        {
+            if (!ValidateURL(website))
+            {
+                throw new ArgumentException("Invalid URL!");
+            }
+
+            return $"Browsing: {website}!";
+        }
+
+        private bool ValidateNumber(string phoneNumber)
+            => phoneNumber.All(c => char.IsDigit(c));
+
+        private bool ValidateURL(string website)
+            => website.All(c => !char.IsDigit(c));
+    }
+}
