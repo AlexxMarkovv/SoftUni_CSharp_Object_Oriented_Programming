a570d8356a5e8167cc2a9eeea15c28d1e70ff880
Alex Markov <langhorne05@gmail.com>
2024-01-08 20:06:47 +0200

    Create StartUp.cs

---
+++ Interfaces and Abstraction LAB and EXERCISES/Shapes/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<IDrawable> figures = new List<IDrawable>();
+
+            IDrawable circle = new Circle(5);
+
+            IDrawable rect = new Rectangle(10, 20);
+
+            figures.Add(circle);
+            figures.Add(rect);
+
+            circle.Draw();
+            rect.Draw();
+        }
+    }
+}
