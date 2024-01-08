83e9b6c37b576757bb308a9ec81d6e33b029827c
Alex Markov <langhorne05@gmail.com>
2024-01-08 20:09:00 +0200

    Create Program.cs

---
+++ Interfaces and Abstraction LAB and EXERCISES/Telephony/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Telephony
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] phoneNumbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] websites = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            ICallable phone;
+
+            foreach (string number in phoneNumbers)
+            {
+                if (number.Length == 10)
+                {
+                    phone = new Smartphone();
+                }
+                else
+                {
+                    phone = new Stationary();
+                }
+
+                try
+                {
+                    Console.WriteLine(phone.Call(number));
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            IBrowsable webDevice = new Smartphone();
+
+            foreach (var website in websites)
+            {
+
+                try
+                {
+                    Console.WriteLine(webDevice.Browse(website));
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
