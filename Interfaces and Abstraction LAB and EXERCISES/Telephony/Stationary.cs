4df6f081e72dd540f392852e68b3f5d21ca30f38
Alex Markov <langhorne05@gmail.com>
2024-01-08 20:09:32 +0200

    Create Stationary.cs

---
+++ Interfaces and Abstraction LAB and EXERCISES/Telephony/Stationary.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Telephony
+{
+    public class Stationary : ICallable
+    {
+        public string Call(string phoneNumber)
+        {
+            if (!ValidateNumber(phoneNumber))
+            {
+                throw new ArgumentException("Invalid number!");
+            }
+
+            return $"Dialing... {phoneNumber}";
+        }
+
+        private bool ValidateNumber(string phoneNumber)
+            => phoneNumber.All(c => char.IsDigit(c));
+    }
+}
