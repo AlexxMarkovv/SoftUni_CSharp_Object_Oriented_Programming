3e69e84bf49ab691d8e032cfb33b5faba8ce6062
Alex Markov <langhorne05@gmail.com>
2024-01-10 22:09:27 +0200

    Create Program.cs

---
+++ Exception Handling LAB and EXERCISE/SquareRoot/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SquareRoot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(num), "Invalid number.");
+                }
+
+                Console.WriteLine(Math.Sqrt(num));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
