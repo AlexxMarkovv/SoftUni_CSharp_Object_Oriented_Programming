ddef0bbafaa262e79c5dbe0605875b368838c96a
Alex Markov <langhorne05@gmail.com>
2024-01-10 22:09:33 +0200

    Create Program.cs

---
+++ Exception Handling LAB and EXERCISE/Sum of Integers/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] inputArrgs = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+int sum = 0;
+
+foreach (string element in inputArrgs)
+{
+	try
+	{
+		int number = int.Parse(element);
+
+		sum += number;
+	}
+	catch (FormatException ex)
+	{
+		Console.WriteLine($"The element '{element}' is in wrong format!");
+	}
+	catch (OverflowException ex)
+	{
+		Console.WriteLine($"The element '{element}' is out of range!");
+	}
+	finally
+	{
+        Console.WriteLine($"Element '{element}' processed - current sum: {sum}");
+    }
+}
+
+Console.WriteLine($"The total sum of all integers is: {sum}");
