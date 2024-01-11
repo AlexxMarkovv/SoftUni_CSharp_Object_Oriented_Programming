9635f55502257e48d2e90d24063b5601fb902e7d
Alex Markov <langhorne05@gmail.com>
2024-01-11 21:04:29 +0200

    Create StartUp.cs

---
+++ Reflection and Attributes LAB and EXERCISE/01.CommandPattern/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace CommandPattern
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //ICommandInterpreter command = new CommandInterpreter();
+            //IEngine engine = new Engine(command);
+            //engine.Run();
+        }
+    }
+    
+}
