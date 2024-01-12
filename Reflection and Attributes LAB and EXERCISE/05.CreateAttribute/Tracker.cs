6d15d034fd19c3dae65110a78610303c5fe9cded
Alex Markov <langhorne05@gmail.com>
2024-01-12 20:03:23 +0200

    Create Tracker.cs

---
+++ Reflection and Attributes LAB and EXERCISE/05.CreateAttribute/Tracker.cs
@@ -0,0 +1,29 @@
+﻿
+using AuthorProblem;
+using System.Reflection;
+
+namespace CreateAttribute
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+
+            MethodInfo[] methods = type.GetMethods((BindingFlags)60);
+
+            foreach (MethodInfo method in methods)
+            {
+                if (method.CustomAttributes.Any(n => n.AttributeType == typeof(AuthorAttribute)))
+                {
+                    object[] attributes = method.GetCustomAttributes(false);
+
+                    foreach (AuthorAttribute attribute in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
