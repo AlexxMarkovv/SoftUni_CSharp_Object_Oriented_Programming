7b0a9e7e283afe02a94a5c6e505bb85a91aa6f83
Alex Markov <langhorne05@gmail.com>
2024-01-14 19:37:25 +0200

    Create ExitCommand.cs

---
+++ Reflection and Attributes LAB and EXERCISE/Command Pattern/Core/Commands/ExitCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace CommandPattern.Core.Commands
+{
+    public class ExitCommand
+    {
+        private const int SuccessExitCode = 0;
+
+        public string Execute(string[] args)
+        {
+            Environment.Exit(SuccessExitCode);
+
+            return null;
+        }
+    }
+}
