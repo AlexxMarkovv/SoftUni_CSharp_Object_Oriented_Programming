987cc09b14e65fb3aef290d1498b7cfb6d3f44aa
Alex Markov <langhorne05@gmail.com>
2024-01-15 14:03:27 +0200

    Create Laptop.cs

---
+++ Reflection and Attributes LAB and EXERCISE/CommandPattern/Laptop.cs
@@ -0,0 +1,23 @@
+﻿namespace CommandPattern
+{
+    public class Laptop
+    {
+        public int model;
+        public int name;
+        public int description;
+        public int type;
+
+        public Laptop(string name, string model, string description)
+        {
+            Name = name;
+            Model = model;
+            Description = description;
+        }
+
+        public string Name { get; set; }
+
+        public string Model { get; set; }
+
+        public string Description { get; set; }
+    }
+}
