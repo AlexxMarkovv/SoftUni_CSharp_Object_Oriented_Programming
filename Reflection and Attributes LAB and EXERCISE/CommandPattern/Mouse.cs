197a26f67729409ea82581a7569dbc60c9da15ee
Alex Markov <langhorne05@gmail.com>
2024-01-15 14:03:36 +0200

    Create Mouse.cs

---
+++ Reflection and Attributes LAB and EXERCISE/CommandPattern/Mouse.cs
@@ -0,0 +1,28 @@
+﻿namespace CommandPattern
+{
+    public class Mouse
+    {
+        public Mouse(string model, int precision)
+        {
+            Model = model;
+            Precision = precision;
+        }
+
+        public string Model { get; set; }
+
+        public int Precision { get; set; }
+
+        [Obsolete]
+        public void MouseMethod()
+        {
+
+        }
+
+        [Obsolete]
+        public void MouseUp() { }
+
+
+        public void NormalMethod() { }
+
+    }
+}
