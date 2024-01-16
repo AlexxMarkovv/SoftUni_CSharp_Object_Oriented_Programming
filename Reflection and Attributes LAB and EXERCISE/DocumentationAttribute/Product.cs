fddd83aeb8fc9b66ddda36ce8dc7d7350427d631
Alex Markov <langhorne05@gmail.com>
2024-01-16 17:26:09 +0200

    Create Product.cs

---
+++ Reflection and Attributes LAB and EXERCISE/DocumentationAttribute/Product.cs
@@ -0,0 +1,16 @@
+﻿
+namespace DocumentationAttributes
+{
+    [Documentation("Product is a class that represents a product!")]
+
+    internal class Product
+    {
+        [Documentation(Documentation = "Print method is a method that prints the product! " +
+            "It is void and accepts 0 parameters!")]
+
+        public void PrintProduct()
+        {
+
+        }
+    }
+}
