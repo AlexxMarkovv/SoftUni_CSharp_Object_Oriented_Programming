bbd07e4eaedfef6f4850f25f886b48aef094c5a7
Alex Markov <langhorne05@gmail.com>
2024-01-16 17:26:18 +0200

    Create Program.cs

---
+++ Reflection and Attributes LAB and EXERCISE/DocumentationAttribute/Program.cs
@@ -0,0 +1,28 @@
+﻿using DocumentationAttributes;
+using System.Reflection;
+
+Type[] types = Assembly.GetExecutingAssembly().GetTypes();
+
+foreach (Type type in types)
+{
+    Console.WriteLine(type.Name);
+
+    DocumentationAttribute doccumentationAttr = type.GetCustomAttribute
+            (typeof(DocumentationAttribute)) as DocumentationAttribute;
+
+    if (doccumentationAttr != null)
+    {
+        Console.WriteLine(doccumentationAttr.Documentation);
+    }
+
+    foreach (var method in type.GetMethods())
+    {
+        doccumentationAttr = method.GetCustomAttribute
+            (typeof(DocumentationAttribute)) as DocumentationAttribute;
+
+        if (doccumentationAttr != null)
+        {
+            Console.WriteLine(doccumentationAttr.Documentation);
+        }
+    }
+}
