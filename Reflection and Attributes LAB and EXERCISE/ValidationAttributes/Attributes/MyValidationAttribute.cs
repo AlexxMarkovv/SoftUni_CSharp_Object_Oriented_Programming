cfe0a9baa908c869f961462d5f6c1ceb421803a4
Alex Markov <langhorne05@gmail.com>
2024-01-17 17:32:34 +0200

    Create MyValidationAttribute.cs

---
+++ Reflection and Attributes LAB and EXERCISE/ValidationAttributes/Attributes/MyValidationAttribute.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ValidationAttributes.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public abstract class MyValidationAttribute : Attribute
+    {
+        public abstract bool IsValid(object obj);
+    }
+}
