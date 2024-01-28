62a2f8d7ea7c3882ab5f203e4149e1237ca81fe3
Alex Markov <langhorne05@gmail.com>
2024-01-28 12:10:54 +0200

    Create PilotRepository.cs

---
+++ C# OOP Exam Preparation/0.56Structure_Skeleton_6.0/Formula1/Repositories/PilotRepository.cs
@@ -0,0 +1,36 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formula1.Repositories
+{
+    public class PilotRepository : IRepository<IPilot>
+    {
+        public PilotRepository()
+        {
+            pilots = new List<IPilot>();
+        }
+
+        private readonly List<IPilot> pilots;
+        public IReadOnlyCollection<IPilot> Models => this.pilots.AsReadOnly();
+
+        public void Add(IPilot model)
+        {
+            pilots.Add(model);
+        }
+
+        public IPilot FindByName(string name)
+        {
+            return pilots.FirstOrDefault(p => p.FullName == name);
+        }
+
+        public bool Remove(IPilot pilot)
+        {
+            return pilots.Remove(pilot); 
+        }
+    }
+}
