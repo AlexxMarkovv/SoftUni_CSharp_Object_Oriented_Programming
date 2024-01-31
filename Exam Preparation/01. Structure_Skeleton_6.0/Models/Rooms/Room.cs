52b7cd1388b5dbbc548f647f4b02181f50a560dd
Alex Markov <langhorne05@gmail.com>
2024-01-31 09:57:10 +0200

    Create Room.cs

---
+++ Exam Preparation/01. Structure_Skeleton_6.0/Models/Rooms/Room.cs
@@ -0,0 +1,47 @@
+﻿using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Models.Rooms
+{
+    public abstract class Room : IRoom
+    {
+        protected Room(int bedCapacity)
+        {
+            BedCapacity = bedCapacity;
+        }
+
+        private int bedCapacity;
+
+        public int BedCapacity
+        {
+            get => bedCapacity;
+            private set => bedCapacity = value;
+        }
+
+        private double pricePerNight = 0;
+
+        public double PricePerNight
+        {
+            get => pricePerNight;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentNullException(ExceptionMessages.PricePerNightNegative);
+                }
+
+                pricePerNight = value;
+            }
+        }
+
+        public void SetPrice(double price)
+        {
+            PricePerNight = price;
+        }
+    }
+}
