ba29f81cfbeafd8ee87bd98adb6dd78ec179fb3c
Alex Markov <langhorne05@gmail.com>
2024-01-31 09:57:35 +0200

    Create HotelRepository.cs

---
+++ Exam Preparation/01. Structure_Skeleton_6.0/Repositories/HotelRepository.cs
@@ -0,0 +1,35 @@
+﻿using BookingApp.Models.Hotels.Contacts;
+using BookingApp.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Repositories
+{
+    public class HotelRepository : IRepository<IHotel>
+    {
+        private readonly List<IHotel> hotels;
+
+        public HotelRepository()
+        {
+            this.hotels = new List<IHotel>();
+        }
+
+        public void AddNew(IHotel model)
+        {
+            hotels.Add(model);
+        }
+
+        public IReadOnlyCollection<IHotel> All()
+        {
+            return hotels.AsReadOnly();
+        }
+
+        public IHotel Select(string hotelName)
+        {
+            return hotels.FirstOrDefault(x => x.FullName == hotelName);
+        }
+    }
+}
