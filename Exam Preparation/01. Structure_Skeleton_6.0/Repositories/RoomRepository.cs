b61c8bd236d53c624ea6057b4e942f51d73bdbd7
Alex Markov <langhorne05@gmail.com>
2024-01-31 09:57:40 +0200

    Create RoomRepository.cs

---
+++ Exam Preparation/01. Structure_Skeleton_6.0/Repositories/RoomRepository.cs
@@ -0,0 +1,37 @@
+﻿using BookingApp.Models.Rooms;
+using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Repositories.Contracts
+{
+    public class RoomRepository : IRepository<IRoom>
+    {
+        private readonly List<IRoom> rooms;
+
+        public RoomRepository()
+        {
+            rooms = new List<IRoom>();
+        }
+
+        public void AddNew(IRoom model)
+        {
+            rooms.Add(model);
+        }
+
+        public IReadOnlyCollection<IRoom> All()
+        {
+            return rooms.AsReadOnly();
+        }
+
+        public IRoom Select(string roomTypeName)
+        {
+            return rooms.FirstOrDefault(x => x.GetType().Name == roomTypeName);
+        }
+    }
+
+}
