0d350acb9422fd786352b49b13f8a2266a10dbc2
Alex Markov <langhorne05@gmail.com>
2024-02-02 11:14:05 +0200

    Create Submarine.cs

---
+++ Exam Preparation/01.1Structure_Skeleton_6.0/NavalVessels/Models/Submarine.cs
@@ -0,0 +1,60 @@
+﻿using NavalVessels.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavalVessels.Models
+{
+    public class Submarine : Vessel, ISubmarine
+    {
+        private const double InitialArmourThickness = 200;
+
+        public Submarine(string name, double mainWeaponCaliber, double speed)
+            : base(name, mainWeaponCaliber, speed, InitialArmourThickness)
+        {
+            SubmergeMode = false;
+        }
+
+        public bool SubmergeMode { get; private set; }
+
+        public void ToggleSubmergeMode()
+        {
+            if (SubmergeMode == false)
+            {
+                MainWeaponCaliber += 40;
+                Speed -= 4;
+            }
+            else
+            {
+                MainWeaponCaliber -= 40;
+                Speed += 4;
+            }
+
+            SubmergeMode = !SubmergeMode;
+        }
+
+        public override void RepairVessel()
+        {
+            ArmorThickness = InitialArmourThickness;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string targetsOutput = !Targets.Any() ? "None" : string.Join(", ", Targets);
+
+            sb.AppendLine($"- {Name}");
+            sb.AppendLine($" *Type: {this.GetType().Name}");
+            sb.AppendLine($" *Armor thickness: {ArmorThickness}");
+            sb.AppendLine($" *Main weapon caliber: {MainWeaponCaliber}");
+            sb.AppendLine($" *Speed: {Speed} knots");
+            sb.AppendLine($" *Targets: {targetsOutput}");
+            sb.AppendLine($" *Submerge mode: {(SubmergeMode ? "ON" : "OFF")}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
