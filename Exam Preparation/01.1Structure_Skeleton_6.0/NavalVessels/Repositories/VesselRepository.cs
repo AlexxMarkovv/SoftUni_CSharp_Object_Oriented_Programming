6530a111f26ef49f63364c4dce689c5c3facdc45
Alex Markov <langhorne05@gmail.com>
2024-02-03 14:26:04 +0200

    Create VesselRepository.cs

---
+++ Exam Preparation/01.1Structure_Skeleton_6.0/NavalVessels/Repositories/VesselRepository.cs
@@ -0,0 +1,36 @@
+﻿using NavalVessels.Models.Contracts;
+using NavalVessels.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavalVessels.Repositories
+{
+    public class VesselRepository : IRepository<IVessel>
+    {
+        public VesselRepository()
+        {
+            vessels = new List<IVessel>();
+        }
+
+        private readonly List<IVessel> vessels;
+        public IReadOnlyCollection<IVessel> Models => vessels.AsReadOnly();
+
+        public void Add(IVessel model)
+        {
+            vessels.Add(model);
+        }
+
+        public bool Remove(IVessel model)
+        {
+            return vessels.Remove(model);
+        }
+
+        public IVessel FindByName(string name)
+        {
+            return vessels.FirstOrDefault(x => x.Name == name);
+        }
+    }
+}
