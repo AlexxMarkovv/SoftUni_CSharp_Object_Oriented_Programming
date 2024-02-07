5b2e9d76a9a98b5425f460f90475122139c40abb
Alex Markov <langhorne05@gmail.com>
2024-02-07 21:35:17 +0200

    Create Race.cs

---
+++ Exam Preparation/0.56Structure_Skeleton_6.0/Formula1/Models/Race.cs
@@ -0,0 +1,87 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formula1.Models
+{
+    public class Race : IRace
+    {
+        public Race(string raceName, int numberOfLaps)
+        {
+            RaceName = raceName;
+            NumberOfLaps = numberOfLaps;
+            pilots = new List<IPilot>();
+        }
+
+        private string raceName;
+        public string RaceName
+        {
+            get { return raceName; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidRaceName, value));
+                }
+                raceName = value;
+            }
+        }
+
+        private int numberOfLaps;
+
+        public int NumberOfLaps
+        {
+            get { return numberOfLaps; }
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidLapNumbers, value));
+
+                }
+                numberOfLaps = value;
+            }
+        }
+
+        private bool tookPlace = false;
+
+        public bool TookPlace
+        {
+            get { return tookPlace; }
+            set { tookPlace = value; }
+        }
+
+        private ICollection<IPilot> pilots;
+        public ICollection<IPilot> Pilots
+        {
+            get => pilots;
+            private set
+            {
+                pilots = value; 
+            }
+        }
+
+        public void AddPilot(IPilot pilot)
+        {
+            Pilots.Add(pilot);
+        }
+
+        public string RaceInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"The {raceName} race has:");
+            sb.AppendLine($"Participants: {pilots.Count}");
+            sb.AppendLine($"Number of laps: {numberOfLaps}");
+            sb.AppendLine($"Took place: {(TookPlace ? "Yes" : "No")}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
