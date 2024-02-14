95e72cca116101ba12372201a71cfc84229880f9
Alex Markov <langhorne05@gmail.com>
2024-02-14 12:32:01 +0200

    Create StartUp.cs

---
+++ Exam Preparation/Students/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace UniversityCompetition
+{
+    using System;
+    using UniversityCompetition.Core;
+    using UniversityCompetition.Core.Contracts;
+    using UniversityCompetition.Models;
+    using UniversityCompetition.Repositories;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            //SubjectRepository repo = new SubjectRepository();
+
+            //repo.AddModel(new TechnicalSubject(555, "test"));
+            //repo.AddModel(new TechnicalSubject(555, "test"));
+            //repo.AddModel(new TechnicalSubject(555, "test"));
+            //repo.AddModel(new TechnicalSubject(555, "test"));
+            //repo.AddModel(new TechnicalSubject(555, "test"));
+
+            //foreach (var item in repo.Models)
+            //{
+            //    Console.WriteLine(item.Id);
+            //}
+
+            IEngine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
