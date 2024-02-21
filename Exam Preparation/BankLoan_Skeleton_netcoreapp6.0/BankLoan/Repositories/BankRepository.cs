a58a5e03d8cbb34c0481c47aeff4b9f2ce3df96f
Alex Markov <langhorne05@gmail.com>
2024-02-21 18:59:05 +0200

    Create BankRepository.cs

---
+++ Exam Preparation/BankLoan_Skeleton_netcoreapp6.0/BankLoan/Repositories/BankRepository.cs
@@ -0,0 +1,41 @@
+﻿using BankLoan.Models.Contracts;
+using BankLoan.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLoan.Repositories
+{
+    public class BankRepository : IRepository<IBank>
+    {
+        public BankRepository()
+        {
+            models = new List<IBank>();
+        }
+
+        private List<IBank> models;
+
+        public IReadOnlyCollection<IBank> Models
+        {
+            get { return models; }
+            private set { }
+        }
+
+        public void AddModel(IBank model)
+        {
+            models.Add(model);
+        }
+
+        public bool RemoveModel(IBank model)
+        {
+            return models.Remove(model);
+        }
+
+        public IBank FirstModel(string name)
+        {
+            return models.FirstOrDefault(x => x.Name == name);
+        }
+    }
+}
