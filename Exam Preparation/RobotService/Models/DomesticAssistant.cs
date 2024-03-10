eb42a09612f9987a93489ab6a3e00f9897133003
Alex Markov <langhorne05@gmail.com>
2024-03-10 12:44:00 +0200

    Create DomesticAssistant.cs

---
+++ Exam Preparation/RobotService/Models/DomesticAssistant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotService.Models
+{
+    public class DomesticAssistant : Robot
+    {
+        private const int DomAssBatteryCapacity = 20_000;
+        private const int DomAssConversionCapacityIndex = 2000;
+
+        public DomesticAssistant(string model)
+            : base(model, DomAssBatteryCapacity, DomAssConversionCapacityIndex)
+        {
+        }
+    }
+}
