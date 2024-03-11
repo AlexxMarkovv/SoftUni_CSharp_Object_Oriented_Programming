14f0bdd52df24674b3dc0ac73cc4cb91d2773695
Alex Markov <langhorne05@gmail.com>
2024-03-11 10:58:35 +0200

    Create IndustrialAssistant.cs

---
+++ Exam Preparation/RobotService/Models/IndustrialAssistant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotService.Models
+{
+    public class IndustrialAssistant : Robot
+    {
+        private const int IndAssBatteryCapacity = 40_000;
+        private const int IndAssConversionCapacityIndex = 5000;
+
+        public IndustrialAssistant(string model)
+            : base(model, IndAssBatteryCapacity, IndAssConversionCapacityIndex)
+        {
+
+        }
+    }
+}
