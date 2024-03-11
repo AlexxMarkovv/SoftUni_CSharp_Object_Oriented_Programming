ad9d5f6ecb2e9b4931c0abbc5b75f284719f660e
Alex Markov <langhorne05@gmail.com>
2024-03-11 10:59:18 +0200

    Create Robot.cs

---
+++ Exam Preparation/RobotService/Models/Robot.cs
@@ -0,0 +1,127 @@
+﻿using RobotService.Models.Contracts;
+using RobotService.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotService.Models
+{
+    public abstract class Robot : IRobot
+    {
+        private readonly List<int> interfaceStandards;
+
+        public Robot(string model, int batteryCapacity, int conversionCapacityIndex)
+        {
+            Model = model;
+            BatteryCapacity = batteryCapacity;
+            BatteryLevel = batteryCapacity;
+            ConvertionCapacityIndex = conversionCapacityIndex;
+            interfaceStandards = new List<int>();
+        }
+
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.ModelNullOrWhitespace);
+                }
+
+                model = value;
+            }
+        }
+
+
+        private int batteryCapacity;
+
+        public int BatteryCapacity
+        {
+            get { return batteryCapacity; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.BatteryCapacityBelowZero);
+                }
+
+                batteryCapacity = value;
+            }
+        }
+
+        private int batteryLevel;
+
+        public int BatteryLevel
+        {
+            get { return batteryLevel; }
+            private set { batteryLevel = value; }
+        }
+
+        private int convertionCapacityIndex;
+
+        public int ConvertionCapacityIndex
+        {
+            get { return convertionCapacityIndex; }
+            private set { convertionCapacityIndex = value; }
+        }
+
+        public IReadOnlyCollection<int> InterfaceStandards => interfaceStandards.AsReadOnly();
+
+        public void Eating(int minutes)
+        {
+            int energy = ConvertionCapacityIndex * minutes;
+
+            if (energy >= BatteryCapacity)
+            {
+                BatteryLevel = BatteryCapacity;
+            }
+            else
+            {
+                BatteryLevel += energy;
+            }
+        }
+
+        public bool ExecuteService(int consumedEnergy)
+        {
+            if (BatteryLevel >= consumedEnergy)
+            {
+                BatteryLevel -= consumedEnergy;
+                return true;
+            }
+
+            return false;
+        }
+
+        public void InstallSupplement(ISupplement supplement)
+        {
+            interfaceStandards.Add(supplement.InterfaceStandard);
+            BatteryCapacity -= supplement.BatteryUsage;
+            BatteryLevel -= supplement.BatteryUsage;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{this.GetType().Name} {Model}:");
+            sb.AppendLine($"--Maximum battery capacity: {BatteryCapacity}");
+            sb.AppendLine($"--Current battery level: {BatteryLevel}");
+            sb.Append("--Supplements installed: ");
+
+            if (!InterfaceStandards.Any())
+            {
+                sb.AppendLine("none");
+            }
+
+            sb.Append($"{string.Join(" ", InterfaceStandards)}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
