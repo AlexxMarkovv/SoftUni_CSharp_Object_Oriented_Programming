1edef5446b7523b03d89c39644931a6077d9d0f4
Alex Markov <langhorne05@gmail.com>
2024-03-12 11:04:34 +0200

    Create SpecializedArm.cs

---
+++ Exam Preparation/RobotService/Models/SpecializedArm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotService.Models
+{
+    public class SpecializedArm : Supplement
+    {
+        private const int SpecializedInterfaceStandard = 10045;
+        private const int SpecializedBatteryUsage = 10_000;
+
+        public SpecializedArm() 
+            : base(SpecializedInterfaceStandard, SpecializedBatteryUsage)
+        {
+        }
+    }
+}
