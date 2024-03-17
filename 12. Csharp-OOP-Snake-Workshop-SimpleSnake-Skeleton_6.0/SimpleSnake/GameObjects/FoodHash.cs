e7fe67311fc2e0009e5c39a5e99f6b3bf5b8f9fa
Alex Markov <langhorne05@gmail.com>
2024-03-17 09:44:56 +0200

    Create FoodHash.cs

---
+++ 12. Csharp-OOP-Snake-Workshop-SimpleSnake-Skeleton_6.0/SimpleSnake/GameObjects/FoodHash.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleSnake.GameObjects
+{
+    public class FoodHash : Food
+    {
+        private const char Symbol = '#';
+        private const int Points = 3;
+
+        public FoodHash(Wall wall)
+            : base(wall, Symbol, Points)
+        {
+
+        }
+    }
+}
